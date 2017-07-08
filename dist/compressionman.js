c7522adbf062eb9f26514a28d96cbe6e85be9c7d
wangkun <850537452@qq.com>
2017-07-09 07:34:16 +0800

    create project

---
+++ dist/compressionman.js
@@ -0,0 +1,19 @@
+/**
+ * CompressionMan
+ * 恶魔在路上(blog.wozir.com)
+ * version : 0.0.1(170709)
+ */
+
+(function($) {
+
+    $.fn.compressionMan = function(options){
+        return new CompressionMan(this, options);
+    };
+
+    var CompressionMan = function(target, options) {
+
+        var me = this;
+
+    }
+
+})(window.Zepto || window.jQuery);
